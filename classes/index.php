ea1fd54ffe8badb62c96ac012f8617493740f250
Zalmoxis245 <47704174+Zalmoxis245@users.noreply.github.com>
2019-11-09 18:16:59 +0200

    Update index.php

--- classes/index.php
+++ classes/index.php
@@ -1,29 +1,30 @@
  <?php
-$servername = "sql100.epizy.com";
-$username = "epiz_24453910";
-$password = "ZenonDaYan15";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "vg";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password ,$dbname);
 
 // Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
 ?>
  <!--Aici incepe a doua secventa php-->
 <?php
-$sql = "SELECT * FROM `basic-users`";
+$sql = "SELECT * FROM basic_users";
 $result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
+if ($result->num_rows > 0)
+ {
     // output data of each row
     while($row = $result->fetch_assoc()) {
         echo "<br> id: ". $row["id"]. " - email: ". $row["email"]. " " . $row["password"] . "<br>";
     }
 } else {
-    echo "0 results";
+    echo "abracadabra";
 }
 
 $conn->close();
