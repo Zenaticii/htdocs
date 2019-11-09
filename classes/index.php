1166cb388c0216b3036520d9d1918a29e84b0753
Zalmoxis245 <47704174+Zalmoxis245@users.noreply.github.com>
2019-11-09 16:31:51 +0200

    Update index.php

--- classes/index.php
+++ classes/index.php
@@ -1,7 +1,30 @@
-<body>
-<?php
-$g = new \Google\Authenticator\GoogleAuthenticator();
-$salt = '7WAO342QFANY6IKBF7L7SWEUU79WL3VMT920VB5NQMW';
-$secret = $username.$salt;echo '<img src="'.$g->getURL($username, 'example.com', $secret).'" />';
+ <?php
+$servername = "sql100.epizy.com";
+$username = "epiz_24453910";
+$password = "ZenonDaYan15";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
 ?>
-</body>
+ <!--Aici incepe a doua secventa php-->
+<?php
+$sql = "SELECT * FROM `basic-users`";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<br> id: ". $row["id"]. " - email: ". $row["email"]. " " . $row["password"] . "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+?> 
